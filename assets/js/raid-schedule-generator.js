94d0650a818c436c195eb04f1276b20587a8dd76
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2025-06-11 22:53:50 -0400

    yolo calendar

--- assets/js/raid-schedule-generator.js
+++ assets/js/raid-schedule-generator.js
@@ -196,7 +196,21 @@ cover-img: /assets/img/raids.webp
         });
     });
 
-    return content + toc + '\n' + mainContent;
+    // Add calendar embed after TOC
+    const calendarEmbed = `
+<div class="calendar-container" style="margin: 20px 0;">
+<iframe src="https://calendar.google.com/calendar/embed?src=66d83074080df7c55ea03673842f6e7b2c2f37ce0c38edf7137603c80e399802%40group.calendar.google.com&ctz=America%2FNew_York" 
+style="border: 0" 
+width="100%" 
+height="600" 
+frameborder="0" 
+scrolling="no">
+</iframe>
+</div>
+
+`;
+
+    return content + toc + calendarEmbed + mainContent;
 }
 
 // Main function
